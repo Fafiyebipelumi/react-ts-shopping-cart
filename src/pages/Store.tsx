e5a37ad97d1571f240c4f4c7ef6643e79217f1da
Oluwapelumi <fafyeepelumi04@gmail.com>
2023-06-09 14:20:07 -0700

    updates

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,6 @@
 import storeItems from '../data/db.json';
 import { Row, Col } from 'react-bootstrap';
+import StoreItem from '../components/StoreItem';
 
 export const Store = () => {
     return (
@@ -7,7 +8,9 @@ export const Store = () => {
             <h1>Store</h1>
             <Row md={2} xs={1} lg={3} className='g-3'>
                 {storeItems.map(item => (
-                    <Col key={item.id}>{JSON.stringify(item)}</Col>
+                    <Col key={item.id}>
+                        <StoreItem {...item} />
+                    </Col>
                 ))}
             </Row>
         </>
